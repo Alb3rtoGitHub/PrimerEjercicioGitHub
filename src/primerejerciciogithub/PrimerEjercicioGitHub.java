3a11d980d319b5d635d2caea479929f682f1dd16
Lfg-LeonardoGomez <lfglfg0804@gmail.com>
2023-08-07 17:42:03 -0300

    update daniel
    saludo final

--- src/primerejerciciogithub/PrimerEjercicioGitHub.java
+++ src/primerejerciciogithub/PrimerEjercicioGitHub.java
@@ -6,6 +6,7 @@ public class PrimerEjercicioGitHub {
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Hola daniel 1000110");
     }
     
 }
